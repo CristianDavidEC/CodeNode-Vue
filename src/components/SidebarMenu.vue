af9220bdac8909aa973ca7be85cbf886c6172909
CristianDavid <cristianescobarcdec@gmail.com>
2022-07-11 00:00:13 -0500

    interface

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -1,7 +1,20 @@
 <template>
-  <aside class="w-64" aria-label="Sidebar">
-    <div class="overflow-y-auto py-4 px-3 rounded-r-lg h-screen bg-blue-900">
-      <ul class="space-y-2"></ul>
+  <aside class="w-56" aria-label="Sidebar">
+    <div class="py-4 px-3 rounded-r-lg h-screen bg-color text-white">
+      <ul class="space-y-2 overflow-y-auto">
+        <li>Variables</li>
+        <li>Math Operations</li>
+        <li>Control</li>
+        <li>Logic operations</li>
+        <li>Cycles</li>
+        <li>Functions</li>
+      </ul>
     </div>
   </aside>
 </template>
+
+<style scoped>
+.bg-color {
+  background-color: rgb(21, 40, 92);
+}
+</style>
